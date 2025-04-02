145caac9f0c26080a48bec280e6b6721106d2d60
InadequateUsername <inadequateusernameinquiries@gmail.com>
2025-04-02 14:39:11 -0400

    Small Fix

--- A DO NOT UPLOAD/db_config.php
+++
@@ -1,9 +0,0 @@
-// /path/outside/webroot/db_config.php
-<?php
-return [
-    'db_server' => 'localhost',
-    'db_username' => 'soulioli_test',
-    'db_password' => '=70mD.VtK6=W',
-    'db_name' => 'soulioli_tracker_db'
-];
-?>
